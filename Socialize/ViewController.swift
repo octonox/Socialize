f38f2c7a157c40bd97d2a3bd9e60f572fbe79690
octonox <victorpetit40@gmail.com>
2019-09-17 09:35:09 +0200

    Initial Commit

---
+++ Socialize/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Socialize
+//
+//  Created by Victor Petit on 17/09/2019.
+//  Copyright © 2019 Victor Petit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
